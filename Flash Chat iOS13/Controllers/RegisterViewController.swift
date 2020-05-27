659d65ecb67d05680de768398cddcc0e41173f8e
Ryan Spishock <22054759+rspishock@users.noreply.github.com>
2020-05-27 15:58:58 -0400

    Add CLTypingLabel and Firebase cocoa pods.  Set up and test user account creation to Firebase.

--- Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -6,14 +6,34 @@
 //  Copyright © 2019 Angela Yu. All rights reserved.
 //
 
+
+// TEST ACCOUNTS:
+// U: test1@test.com  P: 123456
+
+
 import UIKit
+import Firebase
 
 class RegisterViewController: UIViewController {
-
+    
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
     
     @IBAction func registerPressed(_ sender: UIButton) {
+        
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let e = error {
+                    // relay e to user on production app
+                    print(e.localizedDescription)  // Provides localized errors based off of users language settings
+                } else {
+                    // Navigate to ChatCiewController
+                    self.performSegue(withIdentifier: "RegisterToChat", sender: self)
+                }
+            }
+            
+        }
+        
     }
     
 }
