2cac48b140392eb917c4447eefef2a3727ef716c
Ryan Spishock <22054759+rspishock@users.noreply.github.com>
2020-06-05 21:13:11 -0400

    Verified user authentication and logout processes.

---
+++ Flash Chat iOS13/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Flash Chat iOS13
+//
+//  Created by Ryan Spishock on 6/5/20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    
+}
